2a7f7a746a13649bb0fca96f46bcaae03bcd22b2
Haardispro <62053026+Haardispro@users.noreply.github.com>
2021-06-06 21:02:05 +0530

    First Commit

---
+++ home.js
@@ -0,0 +1,52 @@
+//console.log("Hello World");
+//alert("Hello Welcome to this website!");
+
+//aslkdfj;
+/*
+var someNumber = 45;
+console.log(someNumber);
+
+var nume = 10;
+nume = nume + 1
+console.log(nume);
+
+var b = 'hello';
+console.log(b);
+*/
+/*
+var number = 5;
+if(number < 10){
+    document.write("Hello this is an if statement");
+    alert("This is an if and else statemant");
+}
+*/
+
+/*
+function fun(){
+    console.log("This is a function");
+    document.write("Hello this is a website!");
+}
+fun();
+
+function greeting(yourName){
+    var final_greeting = "Hello " + yourName;
+    console.log(final_greeting);
+}
+var name = prompt("What is your name?");
+greeting(name);
+
+function sumNumbers(num1, num2){
+    var result = num1 + num2;
+    console.log(result);
+}
+
+sumNumbers(1, 2);
+*/
+
+
+let num = 0
+while(num < 100){
+    num = num + 1
+    console.log(num);
+}
+
